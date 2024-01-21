f9fb685accd4e48ca0588890479cbc8a2a4d34ee
Gnome101 <polygongome@gmail.com>
2024-01-21 01:28:35 -0700

    Credit Manager is now deploytable

--- deploy/00-deploy-test.js
+++ deploy/00-deploy-test.js
@@ -4,18 +4,24 @@ module.exports = async function ({ getNamedAccounts, deployments }) {
   const { deployer } = await getNamedAccounts();
   const chainId = network.config.chainId;
   console.log("Chain", chainId);
+  const contracts = require("../Addresses");
 
   log("------------------------------------------------------------");
   let args;
 
   const timeStamp = (await ethers.provider.getBlock("latest")).timestamp;
   console.log(timeStamp);
-  args = [1234];
-  const Test = await deploy("Test", {
+  args = [
+    contracts.Sepolia.GHO,
+    contracts.Sepolia.Pool,
+    contracts.Sepolia.SGHODebt,
+    contracts.Sepolia.VGHODebt,
+  ];
+  const CreditManager = await deploy("CreditManager", {
     from: deployer,
     args: args,
     log: true,
     blockConfirmations: 2,
   });
 };
-module.exports.tags = ["All", "Test"];
+module.exports.tags = ["All", "CreditManager"];
